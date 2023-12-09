04e0b8c816c9c8147ebc8e11e3ada5db2fd61ddc
GintarasBeskajev <100523608+GintarasBeskajev@users.noreply.github.com>
2023-12-09 23:19:51 +0200

    Frontend initial push.

---
+++ AuctionFinderFront/src/app/category-modal/category-modal.component.ts
@@ -0,0 +1,60 @@
+import { Component, Inject } from '@angular/core';
+import { ApiService } from '../api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { AuthService } from '../auth.service';
+import { ModalService } from '../modal.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-category-modal',
+  templateUrl: './category-modal.component.html',
+  styleUrl: './category-modal.component.scss'
+})
+
+export class CategoryModalComponent {
+  categoryId: any;
+  category: any;
+  authenticated: boolean = false;
+  initialized: boolean = false;
+  isAdmin: boolean = false;
+
+  constructor(private apiService: ApiService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private datePipe: DatePipe,
+              private authService : AuthService,
+              private modalService : ModalService,
+              private snackBar: MatSnackBar,
+              @Inject(MAT_DIALOG_DATA) public data: any ) {}
+
+  ngOnInit(): void {
+
+    if(this.authService.isAdmin()){
+      this.isAdmin = true;
+    }else{
+      this.router.navigate(['/Login']);
+    }
+
+    this.categoryId = this.data.categoryId;
+
+    this.apiService.getCategory(this.categoryId).subscribe((data) => {
+      this.category = data;
+      this.initialized = true;
+    });
+  }
+
+  onEditClick(){
+    this.modalService.closeModal();
+    this.modalService.openEditCategory(this.categoryId);
+  }
+
+  onDeleteClick(){
+    this.apiService.deleteCategory(this.categoryId).subscribe(response => {
+      this.router.navigate(['/Categories']).then(() => {
+        window.location.reload();
+      });
+    });
+  }
+}
---
+++ AuctionFinderFront/src/app/create-auction/create-auction.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-create-auction',
+  templateUrl: './create-auction.component.html',
+  styleUrl: './create-auction.component.scss'
+})
+export class CreateAuctionComponent {
+  name: any;
+  description: any;
+  endDate: any;
+  startDate: any;
+  categoryId: any;
+  isButtonDisabled: boolean = true;
+  categories: any;
+
+  constructor(private apiService: ApiService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private datePipe: DatePipe,
+              private authService: AuthService ) {}
+
+  ngOnInit(): void {
+
+    if(!this.authService.isAuthenticated()){
+      this.router.navigate(['/Login']).then(() => {
+        window.location.reload();
+      });
+    }
+
+    this.apiService.getCategories().subscribe((data) => {
+      this.categories = data;
+    });
+  }
+
+  updateButtonState() {
+    if(this.name != null && this.description != null && this.endDate != null && this.categoryId != null &&
+        this.name.length != 0 && this.description.length != 0 && this.endDate.length != 0 && this.categoryId.length != 0){
+      this.isButtonDisabled = false;
+    }else{
+      this.isButtonDisabled = true;
+    }
+  }
+
+  onPress(){
+    let endDateObject = new Date(this.endDate);
+    endDateObject.setHours(endDateObject.getHours() - 2);
+    this.endDate = this.datePipe.transform(endDateObject, 'yyyy-MM-ddTHH:mm:ss')!;
+
+    let startDateObject = new Date();
+    startDateObject.setHours(startDateObject.getHours() - 2);
+    this.startDate = this.datePipe.transform(startDateObject, 'yyyy-MM-ddTHH:mm:ss')!;
+
+    this.endDate = this.endDate + 'Z';
+    this.startDate = this.startDate + 'Z';
+
+    const data = { name: this.name, description: this.description, startDate: this.startDate, endDate: this.endDate };
+    this.apiService.createAuction(data, this.categoryId).subscribe(response => {
+      this.router.navigate(['/MyAuctions']).then(() => {
+        window.location.reload();
+      });
+    });
+  }
+}
