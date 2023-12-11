051768ed912f215d95437f4e1282590d4fe8ee30
GintarasBeskajev <100523608+GintarasBeskajev@users.noreply.github.com>
2023-12-11 13:22:40 +0200

    CORS updated.

--- AuctionFinder/AuctionFinder/Program.cs
+++ AuctionFinder/AuctionFinder/Program.cs
@@ -59,7 +59,7 @@ builder.Services.AddCors(options =>
             //http://localhost:4200
             //https://auction-finder-frontend-9lqyri67u-gintaras-projects.vercel.app
             //https://auction-finder-frontend.vercel.app
-            builder.WithOrigins("https://auction-finder-frontend.vercel.app")
+            builder.WithOrigins("http://localhost:4200")
                    .AllowAnyHeader()
                    .AllowAnyMethod()
                    .AllowCredentials();
